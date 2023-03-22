ee53323f85782455def504081e2b0b3698f38b82
suvrajeet01 <suvrajeetbanerjee34767@gmail.com>
2023-03-22 22:06:45 +0530

    stackup.dev

--- 11.mobile-app-dev-with-react-native/2.furniture-app/furniture-app/App.js
+++ 11.mobile-app-dev-with-react-native/2.furniture-app/furniture-app/App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import IndexPage from './pages/IndexPage';
+import DetailPage from './pages/DetailPage';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+     <Stack.Screen name="Index" component={IndexPage} options={{headerShown:false}}/>
+     <Stack.Screen name="DetailPage" component={DetailPage} options={{headerShown:false}}/>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
