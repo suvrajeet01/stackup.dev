1ecf0b8f602d1f9a6cc6e01181416f9a9e1d5dd2
suvrajeet01 <suvrajeetbanerjee34767@gmail.com>
2023-03-24 02:41:23 +0530

    mobile-app-dev-with-react-native

---
+++ 11.mobile-app-dev-with-react-native/3.blog-app/blog-app/App.js
@@ -0,0 +1,29 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import IndexPage from './pages/IndexPage';
+import BlogPage from './pages/BlogPage';
+
+ 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+       <NavigationContainer>
+        <Stack.Navigator>
+         <Stack.Screen name="Index" component={IndexPage} options={{headerShown:false}}/>
+         <Stack.Screen name="BlogPage" component={BlogPage} options={{ headerShown: false }} />
+        </Stack.Navigator>
+       </NavigationContainer>
+        
+      );
+
+  }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
