68147b154f7d4f41f00d202d8be6ce880d3d5d37
Suvrajeet Banerjee <sjb.blackhat@gmail.com>
2023-03-31 07:26:09 +0000

    mint-nft.storage

--- 13.decentralizing-storage-for-web3-with-filecoin/3.mint-nft.storage/filecoin-quest4/scripts/mint-nft.mjs
+++ 13.decentralizing-storage-for-web3-with-filecoin/3.mint-nft.storage/filecoin-quest4/scripts/mint-nft.mjs
@@ -1,7 +1,15 @@
-const CONTRACT_ADDRESS = "PASTE_CONTRACT_ADDRESS";
-const META_DATA_URL = "PASTE_URL_HERE";
+const CONTRACT_ADDRESS = "0xe77781015baf4408616720FFa99E695FAc5f68aB";
+const META_DATA_URL = "ipfs://bafyreiccwchmgbjhaobi74xwpz3pkcg3iwfyktk6fv475wfae7wnnrodi4/metadata.json";
 
 // paste function here
+
+// Function to mint NFT
+async function mintNFT(contractAddress, metaDataURL) {
+  const ExampleNFT = await ethers.getContractFactory("ExampleNFT");
+  const [owner] = await ethers.getSigners();
+  await ExampleNFT.attach(contractAddress).mintNFT(owner.address, metaDataURL);
+  console.log("NFT minted to: ", owner.address);
+ }
 
 mintNFT(CONTRACT_ADDRESS, META_DATA_URL)
   .then(() => process.exit(0))
